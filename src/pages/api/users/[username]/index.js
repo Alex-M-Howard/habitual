d4aadde13b7729063ee7a249d4273045b8872124
Alex Howard <alex-m-howard@pm.me>
2023-03-18 19:33:07 -0400

    User API GET, PUT, DELETE routes added

--- src/pages/api/users/[username]/index.js
+++ src/pages/api/users/[username]/index.js
@@ -1,24 +1,55 @@
-import { UnauthorizedError } from "@/helpers/apiError";
-import { authenticateJWT } from "@/middleware/auth";
+import { authenticateJWT, ensureLoggedIn, ensureCorrectUser } from "@/middleware/auth";
+import User from "@/models/user";
+const jsonschema = require("jsonschema");
+const userUpdateSchema = require("../../../../models/schemas/userUpdate.json");
+
 
 export default async (req, res) => {
+  let token, user, username, response;
+
+  // Middleware Checks
   try {
-    let token = req.rawHeaders[req.rawHeaders.indexOf('Authorization') + 1]
-    const user = authenticateJWT(token);
+    token = ensureLoggedIn(req)
+    user = authenticateJWT(token);
+    username = ensureCorrectUser(user, req)
+    
+    if (!username) throw new Error();
+    
   } catch (error) {
     return res.status(401).json({ error: "Unauthorized"})
   }
 
+
+  // Request Method Switch
   switch (req.method) {
-    case "GET":      
-      return res.status(200).json({ message: "Get user" });
+    case "GET":
+      response = await User.get(user.email);
 
+      if (response.error) return res.status(400).json(response);
+      return res.status(200).json( response );
+
+    
     case "PUT":
-      return res.status(200).json({ message: "Update user" });
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
 
+      if (!validator.valid) {
+        const errs = validator.errors.map((error) => error.stack);
+        return res.status(400).json({ errors: errs });
+      }
+      
+      response = await User.update(user.email, req.body)
+
+      if (response.error) return res.status(400).json(response);
+      return res.status(200).json( response );
+
+    
     case "DELETE":
-      return res.status(200).json({ message: "Delete user" });
+      response = await User.remove(user.email)
 
+      if (response.error) return res.status(400).json(response)
+      return res.status(200).json( response );
+
+    
     default:
       return res.status(405).json({ error: "Method not allowed" });
   }
