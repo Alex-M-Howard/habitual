c5b46e773965d5da7769ea083a9e32a328967cd6
Alex Howard <alex-m-howard@pm.me>
2023-03-18 16:55:29 -0400

    Added starter authenticateJWT to route

--- src/pages/api/users/[username]/index.js
+++ src/pages/api/users/[username]/index.js
@@ -1,6 +1,16 @@
-export default async function handler(req, res) {
+import { UnauthorizedError } from "@/helpers/apiError";
+import { authenticateJWT } from "@/middleware/auth";
+
+export default async (req, res) => {
+  try {
+    let token = req.rawHeaders[req.rawHeaders.indexOf('Authorization') + 1]
+    const user = authenticateJWT(token);
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized"})
+  }
+
   switch (req.method) {
-    case "GET":
+    case "GET":      
       return res.status(200).json({ message: "Get user" });
 
     case "PUT":
@@ -12,4 +22,4 @@ export default async function handler(req, res) {
     default:
       return res.status(405).json({ error: "Method not allowed" });
   }
-}
+};
