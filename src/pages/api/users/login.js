6fbf788c8039281d58d3534dab58b3143e8fbbba
Alex Howard <alex-m-howard@pm.me>
2023-03-18 12:08:21 -0400

    added login to user API

--- src/pages/api/users/login.js
+++ src/pages/api/users/login.js
@@ -1,10 +1,27 @@
+const User = require("../../../models/user");
+const jsonschema = require("jsonschema");
+const userAuthSchema = require("../../../models/schemas/userAuth.json");
+const { createToken } = require("../../../helpers/tokens");
 
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
-      return res.status(200).json({message: "LOGIN USER"});
+      try {
+        const validator = jsonschema.validate(req.body, userAuthSchema);
+
+        if (!validator.valid) {
+          const errs = validator.errors.map((error) => error.stack);
+          return res.status(400).json({ errors: errs });
+        }
+
+        const user = await User.authenticate(req.body);
+        const token = createToken(user);
+        return res.status(201).json({ user, token });
+      } catch (error) {
+        return res.status(400).json({ errors: error });
+      }
 
     default:
-      return res.status(405).json({ error: "Method not allowed" });
+      return res.status(405).json({ ERROR: "Not allowed" });
   }
 }
