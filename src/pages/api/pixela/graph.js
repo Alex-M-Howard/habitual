0290f12c8139e3e5eed7fd32317c9ebff7cf4987
Alex Howard <alex-m-howard@pm.me>
2023-03-17 23:11:34 -0400

    Added skeleton pixela API routes

---
+++ src/pages/api/pixela/graph.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      return res.status(200).json({message: "Get graph SVG and definitions"});
+
+    case "POST":
+      return res.status(200).json({ message: "Create new pixela graph" });
+
+    case "PUT":
+      return res.status(200).json({ message: "Updating pixela graph" });
+
+    case "DELETE":
+      return res.status(200).json({ message: "Delete pixela graph" });
+
+    default:
+      return res.status(405).json({ error: "Method not allowed" });
+  }
+}
