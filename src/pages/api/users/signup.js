ed4bf9f63e9093043470cda3665b8253822318f4
Alex Howard <alex-m-howard@pm.me>
2023-03-18 12:08:28 -0400

    added user signup to API

--- src/pages/api/users/signup.js
+++ src/pages/api/users/signup.js
@@ -1,9 +1,28 @@
+const User = require("../../../models/user");
+const jsonschema = require("jsonschema");
+const userNewSchema = require("../../../models/schemas/userNew.json");
+const { createToken } = require("../../../helpers/tokens");
+
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
-      return res.status(200).json({ message: "SIGNUP USER" });
+      try {
+        const validator = jsonschema.validate(req.body, userNewSchema);
+
+        if (!validator.valid) {
+          const errs = validator.errors.map(error => error.stack);
+          return res.status(400).json({errors: errs})
+        }
+
+        const user = await User.register(req.body);
+        const token = createToken(user);
+        return res.status(201).json({ user, token });
+      } catch (error) {
+          return res.status(400).json({ errors: error });
+      }
+
 
     default:
-      return res.status(405).json({ error: "Method not allowed" });
+      return res.status(405).json({ ERROR: "Not allowed" });
   }
 }
