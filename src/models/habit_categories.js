9bd82b00b81b063bdaf0d45129426e70d4079124
Alex Howard <alex-m-howard@pm.me>
2023-03-19 16:09:41 -0400

    Added habit_category model

--- src/models/habit_categories.js
+++ src/models/habit_categories.js
@@ -27,27 +27,26 @@ class HabitCategories {
     return { habitCategories: result.rows };
   }
 
-  /**
+  /** Add a new habit/category combo
    *
-   * @param {habitId, categoryId} Adds a new habit_category combo
+   * @param {habitId, categoryId}
    * @returns {habitCategories: {habitId, habitName, categoryId, categoryName, permanent}}
    */
   static async add({ habitId, categoryId }) {
-    const existingHabit = await db.query(
-      `SELECT * FROM habits WHERE id = $1`,
-      [habitId]
-    );
+    const existingHabit = await db.query(`SELECT * FROM habits WHERE id = $1`, [
+      habitId,
+    ]);
 
-    if (existingHabit.rows.length < 1) return { error: "Habit doesn't exist. No operations completed." };
-
+    if (existingHabit.rows.length < 1)
+      return { error: "Habit doesn't exist. No operations completed." };
 
     const existingCategory = await db.query(
       `SELECT * FROM categories WHERE id = $1`,
       [categoryId]
     );
 
-    if (existingCategory.rows.length < 1) return { error: "Category doesn't exist. No operations completed." };
-
+    if (existingCategory.rows.length < 1)
+      return { error: "Category doesn't exist. No operations completed." };
 
     const existingHabitCategory = await db.query(
       `SELECT *
@@ -56,8 +55,8 @@ class HabitCategories {
       [habitId, categoryId]
     );
 
-    if (existingHabitCategory.rows.length > 0) return { error: "Habit/Category combo already exists." };
-  
+    if (existingHabitCategory.rows.length > 0)
+      return { error: "Habit/Category combo already exists." };
 
     const result = await db.query(
       `INSERT INTO habit_categories (habit_id, category_id)
@@ -79,55 +78,25 @@ class HabitCategories {
    * Also has to check if habits are using category (In case of duplicate adds)
    */
 
-  static async remove({ id }) {
-    const existingCategories = await db.query(
-      `SELECT
-          id,
-          name,
-          do_not_delete AS "doNotDelete" 
-       FROM categories WHERE id = $1`,
-      [id]
-    );
-    console.log(existingCategories);
-    if (existingCategories.rows.length < 1) {
-      return { error: "Category not found. No operation completed." };
-    }
+  static async remove({ habitId, categoryId }) {
 
-    if (existingCategories.rows[0].doNotDelete) {
-      return {
-        error: "Category has permanent status. No operation completed.",
-      };
-    }
+    const existingHabitCategory = await db.query(`SELECT * FROM habit_categories WHERE habit_id = $1 AND category_id = $2`, [habitId, categoryId]);
+    if (existingHabitCategory.rows.length < 1) return { error: "Habit/Category combo doesn't exist. No operations completed." };
 
-    const habitCategories = await db.query(
-      `SELECT
-          habit_id AS "habitId",
-          category_id AS "categoryId"
-       FROM habit_categories
-       WHERE category_id = $1`,
-      [id]
+    if(existingHabitCategory.rows[0].do_not_delete) return { error: "Habit/Category has permanent status. No operation completed." };
+
+    const result = await db.query(
+      `
+      DELETE
+      FROM habit_categories
+      WHERE habit_id = $1 AND category_id = $2
+      RETURNING habit_id, category_id
+    `,
+      [habitId, categoryId]
     );
 
-    if (habitCategories.rows.length > 0) {
-      return {
-        error: "Category still in use by habit. No operation completed.",
-      };
-    }
 
-    let result = await db.query(
-      `DELETE
-           FROM categories
-           WHERE id = $1
-           RETURNING id`,
-      [id]
-    );
-    const categories = result.rows[0];
-
-    if (!categories) {
-      return { error: `Category ID: ${id} not found. Delete unsuccessful` };
-    }
-
-    return { response: `Categories ID: ${id} successfully deleted.` };
+    return { response: `Habit ID: ${habitId}/Category ID: ${categoryId} successfully deleted.` };
   }
 }
 module.exports = HabitCategories;
