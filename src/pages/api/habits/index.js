d0954c084a9f846b57e6e10490a75e8ea7698fe5
Alex Howard <alex-m-howard@pm.me>
2023-03-18 20:55:57 -0400

    Added get and post route to api/habits/

--- src/pages/api/habits/index.js
+++ src/pages/api/habits/index.js
@@ -1,17 +1,42 @@
+import Habits from "@/models/habits";
+const jsonschema = require("jsonschema");
+const habitNewSchema = require("../../../models/schemas/habitNew.json");
+
 export default async function handler(req, res) {
+  let response;
+  
   switch (req.method) {
     case "GET":
-      return res.status(200).json({ message: "Get all habits" });
+      response = await Habits.findAll();
+      return res.status(200).json(response);
 
+    
+    
     case "POST":
-      return res.status(200).json({ message: "Add habit" });
+      const validator = jsonschema.validate(req.body, habitNewSchema);
 
+      if (!validator.valid) {
+        const errs = validator.errors.map((error) => error.stack);
+        return res.status(400).json({ errors: errs });
+      }
+      
+      response = await Habits.add(req.body);
+
+      if (response.error) return res.status(400).json(response);
+      return res.status(200).json(response);
+
+    
+    
     case "PUT":
       return res.status(200).json({ message: "Update habit" });
 
+    
+    
     case "DELETE":
       return res.status(200).json({ message: "Delete habit" });
 
+    
+    
     default:
       return res.status(405).json({ error: "Method not allowed" });
   }
