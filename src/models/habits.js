acb1f7088f25cb5da28d350cbd6248c713e9dd18
Alex Howard <alex-m-howard@pm.me>
2023-03-19 10:55:15 -0400

    Added habit remove method in model

--- src/models/habits.js
+++ src/models/habits.js
@@ -8,7 +8,7 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 class Habits {
   /** Find all habits.
    *
-   * Returns [{ id, name }, ...]
+   * Returns {habits: [{ id, name }, ...]}
    **/
 
   static async findAll() {
@@ -24,6 +24,12 @@ class Habits {
     return { habits: result.rows };
   }
 
+
+  /**
+   * 
+   * @param {name} Adds a new habit 
+   * @returns {habits: {id, name}}
+   */
   static async add({ name }) {
     const existingHabit = await db.query(
       `SELECT * FROM habits WHERE name = $1`,
@@ -38,31 +44,64 @@ class Habits {
     const result = await db.query(
       `INSERT INTO habits (name)
     VALUES ($1)
-    RETURNING *
+    RETURNING id, name
     `,
       [name]
     );
 
-    return { habits: result.rows };
+    return { habits: result.rows[0] };
   }
 
-  /** Delete given user from database; returns undefined. */
+  /** Delete habit from DB. This has to check if it is allowed to be deleted
+   * Also has to check if other users are using habit (In case of duplicate adds)
+   */
 
-  // static async remove(email) {
-  //   let result = await db.query(
-  //     `DELETE
-  //          FROM users
-  //          WHERE email = $1
-  //          RETURNING email`,
-  //     [email]
-  //   );
-  //   const user = result.rows[0];
+  static async remove({id}) {
+    const existingHabit = await db.query(
+      `SELECT
+          id,
+          name,
+          do_not_delete AS "doNotDelete" 
+       FROM habits WHERE id = $1`,
+      [id]
+    );
+      console.log(existingHabit)
+    if (existingHabit.rows.length < 1) {
+      return { error: "Habit not found. No operation completed." };
+    }
 
-  //   if (!user) {
-  //     return {error: `${email} not found. Delete unsuccessful`}
-  //   }
+    if (existingHabit.doNotDelete) {
+      return { error: "Habit has permanent status. No operation completed." };
+    }
 
-  //   return { response: `User ${email} successfully deleted.` };
-  // }
+    const userHabits = await db.query(
+      `SELECT
+          user_id AS "userId",
+          habit_id AS "habitId"
+       FROM user_habits
+       WHERE habit_id = $1`,
+      [id]
+    );
+
+    if (userHabits.rows.length > 0) {
+      return { error: "Habit still in use by user. No operation completed." };
+    }
+
+
+    let result = await db.query(
+      `DELETE
+           FROM habits
+           WHERE id = $1
+           RETURNING id`,
+      [id]
+    );
+    const habit = result.rows[0];
+
+    if (!habit) {
+      return {error: `Habit: ID-${id} not found. Delete unsuccessful`}
+    }
+
+    return { response: `Habit: ID-${id} successfully deleted.` };
+  }
 }
 module.exports = Habits;
