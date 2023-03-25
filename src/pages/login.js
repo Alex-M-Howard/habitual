85976f714260efe35459c9af8d3c28da329776e3
Alex Howard <alex-m-howard@pm.me>
2023-03-25 08:58:20 -0400

    Added API call to login user and store into Redux store

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,62 +1,72 @@
-// import React, { useState, useContext } from "react";
-// import Form from "@/components/Form";
-// import { Grid, Alert, AlertTitle, Typography } from "@mui/material";
-// import JoblyApi from "@/API";
-// import {useRouter} from "next/router";
+import React, { useState } from "react";
+import Form from "@/components/Form";
+import { Alert, AlertTitle, Grid, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import axios from "axios";
+import useMessageTimer from "@/hooks/useAlerts";
 
-// function Login() {
-//   const [error, setError] = useState(null);
-//   const router = useRouter();
+function Login() {
+  const [error, setError] = useState(null);
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [hidden, hide] = useMessageTimer(error, 3000);
 
-//   const fields = [
-//     { name: "email", label: "Email" },
-//     { name: "password", label: "Password" },
-//   ];
+  const fields = [
+    { name: "email", label: "Email" },
+    { name: "password", label: "Password" },
+  ];
 
-//   const initialValues = {
-//     email: "",
-//     password: ""
-//   };
+  const initialValues = {
+    email: "",
+    password: "",
+  };
 
-//   const handleSubmit = async (formData) => {
-//     if (error) setError(null);
+  const handleSubmit = async (formData) => {
+    if (error) setError(null);
 
-//     let res = await JoblyApi.loginUser(formData);
+    try {
+      const { data } = await axios.post("/api/login", formData);
+      dispatch({ type: "LOGIN", payload: data });
+      await router.push("/");
+    } catch (error) {
+      setError("Email/Password is incorrect.");
+      hide(1);
+    }
+  };
 
-//     if (res.error) {
-//       setError(res.error);
-//     } else {
-//       loginUser(formData.email, res.token);
-//       router.push('/')
-//     }
-//   };
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <div
+        style={{
+          height: "150px",
+          overflow: "hidden",
+          opacity: hidden ? 1 : 0,
+          transition: "opacity 0.3s ease-in-out",
+        }}
+      >
+        <Alert sx={{ m: 2, width: "350px" }} severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      </div>
+      <Typography align="center" variant="h3" sx={{ mt: 5 }}>
+        Login
+      </Typography>
 
-//   return (
-//     <Grid
-//       container
-//       direction="column"
-//       justifyContent="center"
-//       alignItems="center">
-//       {error !== null ? (
-//         <Alert
-//           sx={{ m: 2, minWidth: "350px", maxWidth: "350px" }}
-//           severity="error">
-//           <AlertTitle>Error</AlertTitle>
-//           {error}
-//         </Alert>
-//       ) : null}
-//       <Typography align="center" variant="h3" sx={{ mt: 5 }}>
-//         Login
-//       </Typography>
+      <Form
+        fields={fields}
+        initialValues={initialValues}
+        handleSubmit={handleSubmit}
+        buttonText="Login"
+      />
+    </Grid>
+  );
+}
 
-//       <Form
-//         fields={fields}
-//         initialValues={initialValues}
-//         handleSubmit={handleSubmit}
-//         buttonText="Login"
-//       />
-//     </Grid>
-//   );
-// }
-
-// export default Login;
+export default Login;
