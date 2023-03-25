c7271a3f91511856cf30eb982c31f01b44a83ad9
Alex Howard <alex-m-howard@pm.me>
2023-03-25 10:01:46 -0400

    Added signup and login components

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,64 +1,76 @@
-// import React, {useState} from "react";
-// import Form from "@/components/Form";
-// import { Grid, Alert, AlertTitle, Typography } from "@mui/material";
-// import JoblyApi from "@/API";
+import React, { useState } from "react";
+import Form from "@/components/Form";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { Alert, AlertTitle, Grid, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import useMessageTimer from "@/hooks/useAlerts";
 
-// function Signup() {
-//   const [error, setError] = useState(null)
+function Signup() {
+  const [error, setError] = useState(null);
+  const [hidden, hide] = useMessageTimer(error, 3000);
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-//   const fields = [
-//     { name: 'username', label: 'Username' },
-//     { name: 'password', label: 'Password' },
-//     { name: 'firstName', label: 'First Name'},
-//     {name: 'lastName', label: 'Last Name'},
-//     {name: 'email', label: 'Email'}
-//   ]
-  
-//   const initialValues = {
-//       username: '',
-//       password: '',
-//       firstName: '',
-//       lastName: '',
-//       email: ''
-//     }
-  
-//   const handleSubmit = async (formData) => {
-//     if (error) setError(null);
+  const fields = [
+    { name: "firstName", label: "First Name" },
+    { name: "lastName", label: "Last Name" },
+    { name: "email", label: "Email" },
+    { name: "password", label: "Password" },
+  ];
 
-//     let res = await JoblyApi.registerUser(formData)
-    
-//     if (res.error) {
-//         setError(res.error);
-//     }
-//   }
+  const initialValues = {
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  };
 
-  
-//   return (
-//     <Grid
-//       container
-//       direction="column"
-//       justifyContent="center"
-//       alignItems="center">
-//       {error !== null ? (
-//         <Alert
-//           sx={{ m: 2, minWidth: "350px", maxWidth: "350px" }}
-//           severity="error">
-//           <AlertTitle>Error</AlertTitle>
-//           {error}
-//         </Alert>
-//       ) : null}
-//       <Typography align="center" variant="h3" sx={{mt: 5}}>
-//         Signup
-//       </Typography>
+  const handleSubmit = async (formData) => {
+    if (error) setError(null);
 
-//       <Form
-//         fields={fields}
-//         initialValues={initialValues}
-//         handleSubmit={handleSubmit}
-//         buttonText="Sign Up"
-//       />
-//     </Grid>
-//   );
-// }
+    try {
+      const { data } = await axios.post("/api/signup", formData);
+      dispatch({ type: "LOGIN", payload: data });
+      await router.push("/");
+    } catch (error) {
+      setError("ERROR");
+      hide(1);
+    }
+  };
 
-// export default Signup;
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <div
+        style={{
+          height: "150px",
+          overflow: "hidden",
+          opacity: hidden ? 1 : 0,
+          transition: "opacity 0.3s ease-in-out",
+        }}
+      >
+        <Alert sx={{ m: 2, width: "350px" }} severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      </div>
+      <Typography align="center" variant="h3" sx={{ mt: 5 }}>
+        Signup
+      </Typography>
+
+      <Form
+        fields={fields}
+        initialValues={initialValues}
+        handleSubmit={handleSubmit}
+        buttonText="Sign Up"
+      />
+    </Grid>
+  );
+}
+
+export default Signup;
