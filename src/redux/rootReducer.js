1ba18aba3f8e330549a8c9ef4920468741228673
Alex Howard <alex-m-howard@pm.me>
2023-03-24 20:23:23 -0400

    Combined user and theme into one rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,35 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import themeReducer from "./themeReducer";
+// rootReducer.js
+import { lightTheme, darkTheme } from "@/config/theme";
 
-const rootReducer = combineReducers({
-  user: userReducer,
-  theme: themeReducer,
-});
+const INITIAL_USER_STATE = { user: {} };
+const INITIAL_THEME_STATE = { theme: 'light' };
+
+function userReducer(state = INITIAL_USER_STATE, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: {} };
+    default:
+      return state;
+  }
+}
+
+function themeReducer(state = INITIAL_THEME_STATE, action) {
+  switch (action.type) {
+    case "CHANGE_THEME":
+        return { ...state, theme: action.payload}
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = (state = {}, action) => {
+  return {
+    user: userReducer(state.user, action),
+    activeTheme: themeReducer(state.theme, action),
+  };
+};
 
 export default rootReducer;
