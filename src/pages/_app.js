ac9332dff82e9992d1673d1c86648de9b8b4f67c
Alex Howard <alex-m-howard@pm.me>
2023-03-25 08:57:22 -0400

    Corrected import of user data from redux store

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,13 @@
 //General Imports
-import { CacheProvider } from "@emotion/react";
+import {CacheProvider} from "@emotion/react";
 import createEmotionCache from "../config/createEmotionCache";
 import rootReducer from "@/redux/rootReducer.js";
-import { Provider, useSelector } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { lightTheme, darkTheme } from "@/config/theme";
+import {Provider, useSelector} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+import {darkTheme, lightTheme} from "@/config/theme";
 
 //MaterialUI Imports
-import { ThemeProvider } from "@mui/material/styles";
+import {ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -17,25 +17,23 @@ import "@fontsource/roboto/700.css";
 //Component Imports
 import NavBar from "@/components/NavBar";
 
-
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
- const store = configureStore({
-   reducer: rootReducer,
- });
+const store = configureStore({
+  reducer: rootReducer,
+});
 
-function ThemeWrapper({children}) {
-  const colorMode = useSelector(store => store.activeTheme.theme);
-  const theme = colorMode === 'light' ? lightTheme : darkTheme;
+function ThemeWrapper({ children }) {
+  const colorMode = useSelector((store) => store.theme.theme);
+  const theme = colorMode === "light" ? lightTheme : darkTheme;
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
-
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  
+
   return (
     <CacheProvider value={emotionCache}>
       <Provider store={store}>
