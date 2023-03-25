81bf6f5bad0c8d0f36576ec0ef2847c00e6580f2
Alex Howard <alex-m-howard@pm.me>
2023-03-25 08:54:59 -0400

    Add custom hook for Alert Timers

---
+++ src/hooks/useAlerts.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function useMessageTimer(initialState = null, delay = 2500) {
+  const [message, setMessage] = useState(initialState);
+
+  useEffect(() => {
+    if (message) {
+      const timeoutId = setTimeout(() => {
+        setMessage(null);
+      }, delay);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [message, delay]);
+
+  return [message, setMessage];
+}
+
+export default useMessageTimer;
