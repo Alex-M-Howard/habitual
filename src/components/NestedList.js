f0a13aa5c72ab20619d8205abc64f7be5cec382d
Alex Howard <alex-m-howard@pm.me>
2023-04-06 09:20:40 -0400

    Added nested list to choose habits to add

--- src/components/NestedList.js
+++ src/components/NestedList.js
@@ -8,10 +8,19 @@ import Collapse from "@mui/material/Collapse";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { TextField } from "@mui/material";
+import uuid4 from "uuid4";
 
-export default function NestedList({ habits, habitCategories }) {
+function NestedList({
+  userHabits,
+  habitCategories,
+  onHabitSelect,
+  customHabit,
+  setCustomHabit,
+}) {
   const [open, setOpen] = React.useState({});
   const [categories, setCategories] = React.useState([]);
+  const [selectedHabitId, setSelectedHabitId] = React.useState(null);
   const { user, token } = useSelector((store) => store.user.loggedIn);
 
   useEffect(() => {
@@ -35,31 +44,75 @@ export default function NestedList({ habits, habitCategories }) {
     setOpen((state) => ({ ...state, [categoryId]: !state[categoryId] }));
   };
 
+  const handleHabitClick = (habitId) => {
+    setSelectedHabitId(habitId);
+    const selectedHabit = habitCategories.find(
+      (habit) => habit.habitId === habitId
+    );
+    if (selectedHabit) {
+      onHabitSelect(selectedHabit.habitName);
+    }
+  };
+
+  const handleOtherCategoryClick = () => {
+    setSelectedHabitId("other");
+    onHabitSelect(""); // Clear any previously selected habit name
+  };
+
   const listCategories = () => {
     return categories.map((category) => {
-      const habits = () => {
+      const habitList = () => {
+        const userHabitIds = userHabits.map((habit) => habit.habitId);
         return habitCategories.map((habit) => {
           if (habit.categoryId === category.id) {
-            return (
-              <Collapse in={open[category.id]} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                  <ListItemButton sx={{ pl: 4 }}>
-                    <ListItemText primary={habit.habitName} />
-                  </ListItemButton>
-                </List>
-              </Collapse>
-            );
+            const isSelected = selectedHabitId === habit.habitId;
+            if (userHabitIds.includes(habit.habitId)) {
+              return (
+                <Collapse in={open[category.id]} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding key={uuid4()}>
+                    <ListItemButton sx={{ pl: 4 }} disabled={true}>
+                      <ListItemText primary={habit.habitName} />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+              );
+            } else {
+              return (
+                <Collapse in={open[category.id]} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding key={uuid4()}>
+                    <ListItemButton
+                      sx={{
+                        pl: 4,
+                        backgroundColor: isSelected
+                          ? "primary.main"
+                          : "transparent",
+                      }}
+                      onClick={() => handleHabitClick(habit.habitId)}
+                    >
+                      <ListItemText
+                        primary={habit.habitName}
+                        primaryTypographyProps={{
+                          color: isSelected
+                            ? "primary.contrastText"
+                            : "inherit",
+                        }}
+                      />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+              );
+            }
           }
         });
       };
 
       return (
-        <div>
+        <div key={uuid4()}>
           <ListItemButton onClick={() => handleClick(category.id)}>
             <ListItemText primary={category.name} />
             {open[category.id] ? <ExpandLess /> : <ExpandMore />}
           </ListItemButton>
-          {habits()}
+          {habitList()}
         </div>
       );
     });
@@ -77,6 +130,24 @@ export default function NestedList({ habits, habitCategories }) {
       }
     >
       {listCategories()}
+      <ListItemButton
+        selected={selectedHabitId === "other"}
+        onClick={handleOtherCategoryClick}
+      >
+        <ListItemText primary="Other" />
+      </ListItemButton>
+      {selectedHabitId === "other" && (
+        <TextField
+          label="Custom habit"
+          variant="outlined"
+          size="small"
+          value={customHabit}
+          onChange={(e) => setCustomHabit(e.target.value)}
+          sx={{ mt: 2, mb: 2 }}
+        />
+      )}
     </List>
   );
 }
+
+export default NestedList;
