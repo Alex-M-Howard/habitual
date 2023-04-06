f8131e175f4eb8e457dd5e8dc314babb1982d2b7
Alex Howard <alex-m-howard@pm.me>
2023-04-06 15:40:30 -0400

    Added update route for Journals

--- src/pages/api/users/[userId]/journal/index.js
+++ src/pages/api/users/[userId]/journal/index.js
@@ -3,11 +3,9 @@ import Journals from "@/models/journals";
 
 const jsonschema = require("jsonschema");
 const journalNewSchema = require("@/models/schemas/journalNew.json");
-const journalGetSchema = require("@/models/schemas/journalGet.json");
 
 export default async function handler(req, res) {
   let token, user, response, validator;
-
   // Middleware Checks
   try {
     token = ensureLoggedIn(req);
@@ -21,15 +19,7 @@ export default async function handler(req, res) {
   // Request Method Switch
   switch (req.method) {
     case "GET":
-      req.query;
-      validator = jsonschema.validate(req.query, journalGetSchema);
-
-      if (!validator.valid) {
-        const errs = validator.errors.map((error) => error.stack);
-        return res.status(400).json({ errors: errs });
-      }
-
-      response = await Journals.getJournals(req.body);
+      response = await Journals.getJournals({ userId: req.query.userId });
       return res.status(200).json(response);
 
     case "POST":
@@ -40,11 +30,20 @@ export default async function handler(req, res) {
         return res.status(400).json({ errors: errs });
       }
 
+      req.body.userId = req.query.userId;
       response = await Journals.add(req.body);
 
       if (response.error) return res.status(400).json(response);
       return res.status(200).json(response);
 
+    case "PUT":
+      req.body.userId = req.query.userId;
+      response = await Journals.update(req.body);
+
+      if (response.error) return res.status(400).json(response);
+      return res.status(200).json(response);
+
+    // TODO - Add delete for journals
     // case "DELETE":
     //   validator = jsonschema.validate(req.body, habit_categoriesDeleteSchema);
 
