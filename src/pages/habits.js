b2a5b603104257fe08a8489a0ad9da460a2878ca
Alex Howard <alex-m-howard@pm.me>
2023-04-06 09:21:49 -0400

    Refactored axios calls to clean up code

--- src/pages/habits.js
+++ src/pages/habits.js
@@ -3,62 +3,68 @@ import { useSelector } from "react-redux";
 import axios from "axios";
 import { Button } from "@mui/material";
 import AddHabit from "@/components/AddHabit";
+import uuid4 from "uuid4";
 
 function Habits() {
   const { user, token } = useSelector((store) => store.user.loggedIn);
-  const [habits, setHabits] = useState([]);
+  const [userHabits, setUserHabits] = useState([]);
+  const [customHabit, setCustomHabit] = useState("");
   const [categories, setCategories] = useState([]);
   const [addShowing, setAddShowing] = useState(false);
 
   useEffect(() => {
     async function getHabits() {
-      const res = await axios({
-        method: "GET",
-        url: `/api/users/${user.id}/habits`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
-
+      const url = `/api/users/${user.id}/habits`;
+      const headers = { Authorization: `Bearer ${token}` };
+      const res = await axios.get(url, { headers });
       return res.data.habits;
     }
 
     async function getCategories() {
-      const res = await axios({
-        method: "GET",
-        url: `/api/habit_categories`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
+      const url = `/api/habit_categories`;
+      const headers = { Authorization: `Bearer ${token}` };
+      const res = await axios.get(url, { headers });
       return res.data.habitCategories;
     }
 
     if (!user) return;
-    getHabits().then((data) => setHabits(data));
+    getHabits().then((data) => setUserHabits(data));
     getCategories().then((data) => setCategories(data));
   }, [user]);
 
   function getHabits() {
-    return habits.map((habit) => {
+    return userHabits.map((habit) => {
       return (
-        <div key={habit.id}>
+        <div key={uuid4()}>
           <h2>{habit.habitName}</h2>
         </div>
       );
     });
   }
 
-  if (!habits) return <div>Loading...</div>;
+  if (!userHabits) return <div>Loading...</div>;
 
   const handleClick = () => {
     setAddShowing(true);
   };
 
+  const handleHabitSelect = (habitName) => {
+    setCustomHabit(habitName);
+  };
+
+  console.log(userHabits);
   if (addShowing) {
-    return <AddHabit habits={habits} categories={categories} />;
+    return (
+      <AddHabit
+        userHabits={userHabits}
+        categories={categories}
+        onHabitSelect={handleHabitSelect}
+        customHabit={customHabit}
+        setCustomHabit={setCustomHabit}
+        setAddShowing={setAddShowing}
+        setUserHabits={setUserHabits}
+      />
+    );
   } else {
     return (
       <div>
