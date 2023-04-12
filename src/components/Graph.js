3049934d73765c64f2cb300e547011a5b4bb435d
Alex Howard <alex-m-howard@pm.me>
2023-04-11 22:40:41 -0400

    Took Chart.js component and made it modular

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,63 +1,44 @@
 import React from "react";
+import { Box } from "@mui/material";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
+  LineElement,
+  PointElement,
   BarElement,
   Title,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Bar, Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  LineElement,
+  PointElement,
   BarElement,
   Title,
   Tooltip,
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Habits Completed by Day of the Week",
-    },
-  },
-};
+function Graph({ data, options, graphType }) {
+  console.log(graphType);
 
-// Replace this with your own data
-const habitsCompleted = {
-  Sunday: 50,
-  Monday: 60,
-  Tuesday: 45,
-  Wednesday: 70,
-  Thursday: 40,
-  Friday: 65,
-  Saturday: 55,
-};
+  const chartComponent =
+    graphType === "bar" ? (
+      <Bar options={options} data={data} />
+    ) : (
+      <Line options={options} data={data} />
+    );
 
-const labels = Object.keys(habitsCompleted);
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Completed Habits",
-      data: labels.map((day) => habitsCompleted[day]),
-      backgroundColor: "rgba(75, 192, 192, 0.5)",
-    },
-  ],
-};
-
-function Graph() {
-  return <Bar options={options} data={data} />;
+  return (
+    <Box width="100%" height="400px" display="flex" justifyContent="center">
+      {chartComponent}
+    </Box>
+  );
 }
 
 export default Graph;
